23ad8e815c58c350f4a2e07ba13a5df3bcc21b13
Yann Raymond <raymondyann581@gmail.com>
2021-10-07 17:59:25 +0200

    initialisation 2

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div>
+    <base-header />
+    <home-page />
+    <base-footer />
+  </div>
+  <script>
+    import  from "./Components/Page/HomePage/Header.vue"
+  </script>
+</template>
---
+++ src/components/Page/Homepage/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="header"> joe le rigolo</div>
+</template>
+
+<script>
+export default {
+  name: "home-page"
+}
+</script>
+
+<style scoped>
+.header{
+  color: red;
+}
+</style>
